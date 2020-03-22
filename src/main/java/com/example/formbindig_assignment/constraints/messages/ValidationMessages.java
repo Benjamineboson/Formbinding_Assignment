1112fd2bec9529d42916cc4b5b2847614bd1833b
Benjamin Boson <benjamineboson@gmail.com>
2020-03-22 18:31:26 +0100

    init commit

---
+++ src/main/java/com/example/formbindig_assignment/constraints/messages/ValidationMessages.java
@@ -0,0 +1,13 @@
+package com.example.formbindig_assignment.constraints.messages;
+
+public class ValidationMessages {
+    public static final String EMAIL_FORMAT_MESSAGE = "Email address format is not valid";
+    public static final String STREET_SIZE_ERROR = "Street names can only contain 2-100 characters";
+    public static final String ZIP_SIZE_ERROR = "Zip codes can only contain 6 characters (123 45)";
+    public static final String CITY_SIZE_ERROR = "City names can only contain 2-100 characters";
+    public static final String HOME_PHONE_SIZE_ERROR = "Home phone numbers can only contain 10 characters (040-123456)";
+    public static final String CELL_PHONE_SIZE_ERROR = "Cell phone numbers can only contain 11 characters (070-1234567)";
+
+
+
+}
